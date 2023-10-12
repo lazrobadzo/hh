1d0e6005dc9fa763a392a85d7d959842c9707eb7
lazrobadzo <144172921+lazrobadzo@users.noreply.github.com>
2023-10-12 07:35:35 -0700

    n
    nn

---
+++ dy.py
@@ -0,0 +1,23 @@
+user_age =input("what,s your age:? l am 33 years old " )
+print(user_age)
+ 
+user_name = input("what is your name? my name is meta")
+print(user_name)
+ 
+usermom_name =input (" mom what is your name:? my name  is gio")
+print(usermom_name)
+ 
+
+userded_name = input ("ded what is your name:? my name is teo")
+print(userded_name)
+
+teo_age= 28
+gio_age= 29
+my_age=33
+
+
+print(teo_age+5)
+
+print(gio_age+5)
+
+print(my_age+5)
